3c71a99fc30ee50d5863eab998f049873c797a7b
jlam <jennylamwork@gmail.com>
2020-03-28 20:07:08 -0700

    added questions

---
+++ assets/css/javascript/app.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: 'What do you use for an array?'
+    choices: {
+      a. '()',
+      b. '""',
+      c. '[]',
+      d. '<>'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Which is a boolean?'
+    choices: {
+      a. '28',
+      b. '[]',
+      c. 'Javascript',
+      d. 'True'
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: 'Where do you put a style CDN?'
+    choices: {
+      a. 'In the head tag',
+      b. 'In the body tag',
+      c. 'In the title tag',
+      d. 'At the bottom of the body tag'
+    },
+    correctAnswer: 'a'
+  }
+]
